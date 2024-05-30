5cd94cf2c88db2c59398c2745281f6bb8d1bc241
Carlo <cruz.carlo.diaz@gmail.com>
2024-05-31 01:09:27 +0800

    FCC Timestamp Microservice solution

--- index.js
+++ index.js
@@ -25,6 +25,45 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api", function (req, res) {
+  let date = new Date().getTime();
+  const unix = date;
+  const utc = new Date(date).toUTCString();
+
+  res.json({ unix, utc });
+})
+
+app.get("/api/:date", function (req, res) {
+  let date_string = req.params.date;
+
+  if (date_string === undefined) {
+    res.json({ error : 'Invalid Date' });
+    return;
+  }
+
+  if (date_string.includes('-') || !isNaN(new Date(date_string))) {
+    if (isNaN(new Date(date_string))) {
+      res.json({ error : 'Invalid Date' });
+      return;
+    }
+    date = new Date(date_string);
+  } else if (!Number.isNaN(Number(date_string))) {
+    if (isNaN(new Date(Number(date_string)))) {
+      res.json({ error : 'Invalid Date' });
+      return;
+    }
+    date = new Date(Number(date_string));
+  } else {
+    res.json({ error : 'Invalid Date' });
+    return;
+  }
+
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+
+  res.json({ unix, utc });
+});
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
